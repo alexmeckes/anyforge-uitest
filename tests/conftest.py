e84ec33c145dab47f922e528566f0a2da9c8e92d
daavoo <daviddelaiglesiacastro@gmail.com>
2025-08-27 15:35:26 +0200

    chore: Increase `AppTest` timeout.

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,6 @@ from streamlit.testing.v1 import AppTest
 @pytest.fixture
 def any_forge_app() -> AppTest:
     app = AppTest.from_file(str(Path(__file__).parent.parent / "streamlit_app.py"))
-    app.run()
+    app.run(timeout=10)
     app.session_state["agents"] = {}
     return app
